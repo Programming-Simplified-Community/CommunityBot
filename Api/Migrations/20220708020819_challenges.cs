5d057333d7708e7aa00c5ee976c4528f212e5c13
Badger 2-3 <jonbraunsma@gmail.com>
2022-07-07 23:16:33 -0400

    added migrations to source control

---
+++ Api/Migrations/20220708020819_challenges.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Migrations
+{
+    public partial class challenges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProgrammingChallenges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Question = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Explanation = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Tip = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgrammingChallenges", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ChallengeReports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProgrammingChallengeId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Points = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChallengeReports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChallengeReports_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ChallengeReports_ProgrammingChallenges_ProgrammingChallengeId",
+                        column: x => x.ProgrammingChallengeId,
+                        principalTable: "ProgrammingChallenges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProgrammingChallengeSubmissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserId = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SubmittedOn = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    DiscordGuildId = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DiscordChannelId = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ProgrammingChallengeId = table.Column<int>(type: "int", nullable: false),
+                    UserSubmission = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgrammingChallengeSubmissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProgrammingChallengeSubmissions_ProgrammingChallenges_Progra~",
+                        column: x => x.ProgrammingChallengeId,
+                        principalTable: "ProgrammingChallenges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProgrammingTests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProgrammingChallengeId = table.Column<int>(type: "int", nullable: false),
+                    Language = table.Column<int>(type: "int", nullable: false),
+                    TestDockerImage = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DockerEntryPoint = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ExecutableFileMountDestination = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProgrammingTests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProgrammingTests_ProgrammingChallenges_ProgrammingChallengeId",
+                        column: x => x.ProgrammingChallengeId,
+                        principalTable: "ProgrammingChallenges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TestResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProgrammingChallengeReportId = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Result = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestResults", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestResults_ChallengeReports_ProgrammingChallengeReportId",
+                        column: x => x.ProgrammingChallengeReportId,
+                        principalTable: "ChallengeReports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChallengeReports_ProgrammingChallengeId",
+                table: "ChallengeReports",
+                column: "ProgrammingChallengeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChallengeReports_UserId",
+                table: "ChallengeReports",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgrammingChallengeSubmissions_ProgrammingChallengeId",
+                table: "ProgrammingChallengeSubmissions",
+                column: "ProgrammingChallengeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProgrammingTests_ProgrammingChallengeId",
+                table: "ProgrammingTests",
+                column: "ProgrammingChallengeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestResults_ProgrammingChallengeReportId",
+                table: "TestResults",
+                column: "ProgrammingChallengeReportId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProgrammingChallengeSubmissions");
+
+            migrationBuilder.DropTable(
+                name: "ProgrammingTests");
+
+            migrationBuilder.DropTable(
+                name: "TestResults");
+
+            migrationBuilder.DropTable(
+                name: "ChallengeReports");
+
+            migrationBuilder.DropTable(
+                name: "ProgrammingChallenges");
+        }
+    }
+}
